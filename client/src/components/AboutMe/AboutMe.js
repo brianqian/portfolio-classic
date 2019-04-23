d979fe21a57bcb56cd13dcc57dfc410701a4f954
BQ <qian.brian@gmail.com>
2019-04-23 15:27:38 -0700

    stragglers

--- client/src/components/AboutMe/AboutMe.js
+++ client/src/components/AboutMe/AboutMe.js
@@ -10,6 +10,20 @@ const Container = styled.div`
     margin: 0;
   }
 `;
+
+const ProfilePic = styled.div`
+  box-shadow: -2px 2px 20px;
+  border-radius: 50%;
+  height: 35vh;
+  width: 35vh;
+  border: 5px solid white;
+  background-image: url(./img/BrianQian.png);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  z-index: 1;
+`;
+
 function AboutMe({ className }) {
   return (
     <Container className={className}>
