588e034a2fb2caffa8399e3f4a318aeabb14121a
BQ <qian.brian@gmail.com>
2019-04-23 15:27:38 -0700

    stragglers

---
+++ client/src/components/AboutMe/AboutMeMain.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+
+width: 100%;
+
+`
+
+
+
+function AboutMeMain() {
+  return (
+    <Container>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque hic ea sed quibusdam, magnam doloribus ad labore architecto, corrupti consectetur, ipsam quae quia ipsum esse dicta aperiam tenetur unde reprehenderit!Lorem Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit doloribus exercitationem voluptas accusantium officia ad omnis blanditiis rerum facere natus consectetur ratione veritatis, eum dicta praesentium quae sunt excepturi quaerat? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque modi amet quidem nobis quisquam nihil culpa magni dolore fugit? Est eaque voluptate odit natus dicta voluptatibus culpa omnis, non quasi!</p>
+    </Container>
+  )
+}
+
+export default AboutMeMain
