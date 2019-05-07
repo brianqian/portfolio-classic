8ed587cbd3998ed0ac0b02e741cf8b915787c2ef
BQ <qian.brian@gmail.com>
2019-05-07 13:33:37 -0700

    added test

--- client/src/components/PortfolioItem/PortfolioItem.test.js
+++ client/src/components/PortfolioItem/PortfolioItem.test.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import PortfolioItem from "./PortfolioItem";
 import { shallow } from "enzyme";
@@ -31,3 +32,38 @@ describe("When Portfolio Item mounts", () => {
   //   expect(wrapper.exists()).toBe(true);
   // });
 });
+=======
+// import React from "react";
+// import PortfolioItem from "./PortfolioItem";
+// import { shallow } from "enzyme";
+// import data from "../../data/PortfolioData";
+
+// const props = data[0];
+
+// describe("When Portfolio Item mounts", () => {
+//   const IntersectionObserver = window.IntersectionObserver;
+//   const observe = jest.fn();
+//   const mockRef = {mock: true};
+
+//   beforeAll(() => {
+//     window.IntersectionObserver = jest.fn(function() {
+//       this.observe = observe;
+//     });
+//     const wrapper = shallow(<PortfolioItem {...props} />, {disableLifecycleMethods: true});
+//     wrapper.getElement().ref(mockRef)
+//     wrapper.instance().componentDidMount();
+//   });
+
+//   afterAll(()=>{
+//     window.IntersectionObserver = IntersectionObserver;
+//   })
+
+//   it('creates and observer on the ref', ()=>{
+//     expect(observe).toBeCalledWith(mockRef)
+//   })
+//   // it("should render without crashing", () => {
+//   //   const wrapper = shallow(<PortfolioItem {...props} />);
+//   //   expect(wrapper.exists()).toBe(true);
+//   // });
+// });
+>>>>>>> a70dccb... changed about me, started testing for portfolioItem
