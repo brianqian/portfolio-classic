e610ecd260cc17b763a4a59c46931e132c238a75
BQ <qian.brian@gmail.com>
2019-05-07 13:20:30 -0700

    fixed links to code and website

--- client/src/pages/Portfolio/PortfolioContainer.js
+++ client/src/pages/Portfolio/PortfolioContainer.js
@@ -10,7 +10,7 @@ const Container = styled.section`
   flex-direction: column;
   text-align: center;
   font-size: 1.2em;
-  padding: 7rem 1rem;
+  padding: 7rem 2rem;
   z-index: 10;
   @media all and (max-width: 900px) {
     text-align: center;
